c5afd3380abdfdb938f54bc9de04e9f314c6ecc0
macbookair <imlazarev@phoenixit.ru>
2025-08-06 23:33:52 +0300

    update

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	todo "todo-app"
+	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
+)
+
+func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %v", err.Error())
+	}
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"todo-app/pkg/service"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllLists)
+			lists.GET("/:id", h.getListById)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group("/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItems)
+				items.GET("/:items_id", h.getItemById)
+				items.PUT("/:items_id", h.updateItem)
+				items.DELETE("/:items_id", h.deleteItem)
+			}
+		}
+	}
+
+	return router
+}
